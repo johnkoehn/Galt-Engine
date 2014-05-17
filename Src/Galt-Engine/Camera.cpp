ff518e0672be62859a1e472791f645acb35365ef
John Koehn <kingkoehn@gmail.com>
2014-05-17 13:36:16 -0500

    Continued work on camera class

--- Src/Galt-Engine/Camera.cpp
+++ Src/Galt-Engine/Camera.cpp
@@ -2,12 +2,12 @@
 
 using namespace GaltE;
 
-Camera::Camera(sf::FloatRect rect)
+Camera::Camera(const sf::FloatRect& rect)
 {
 	view.reset(rect);
 }
 
-Camera::Camera(sf::Vector2f center, sf::Vector2f size)
+Camera::Camera(const sf::Vector2f& center, const sf::Vector2f& size)
 {
 	view.setCenter(center);
 	view.setSize(size);
@@ -18,7 +18,42 @@ Camera::Camera()
 
 }
 
-void Camera::setView(sf::FloatRect rect)
+void Camera::setView(const sf::FloatRect& rect)
 {
 	view.reset(rect);
+}
+
+void Camera::setCenter(const sf::Vector2f& coordinates)
+{
+	view.setCenter(coordinates);
+}
+
+void Camera::setSize(const sf::Vector2f& size)
+{
+	view.setSize(size);
+}
+
+void Camera::setRotation(float angle)
+{
+	view.setRotation(angle);
+}
+
+void Camera::rotate(float angle)
+{
+	view.rotate(angle);
+}
+
+void Camera::move(float deltaX, float deltaY)
+{
+	view.move(deltaX, deltaY);
+}
+
+void Camera::setViewport(sf::FloatRect& rect)
+{
+	//test to make sure all componenets are between 1 and 0 inclusive
+	if (!(rect.left <= 1) || !(rect.left >= 0))
+	{
+		rect.left = 0;
+		std::cerr << "View port value greater then 1 or less then 0";
+	}
 }
--- Src/Galt-Engine/Camera.h
+++ Src/Galt-Engine/Camera.h
@@ -21,32 +21,66 @@ namespace GaltE
 		* two coordinates are the global top left x and y positions of the camera.
 		* The next to values in the sf::FloatRect describe the width and height
 		**/
-		Camera(sf::FloatRect rect);
+		Camera(const sf::FloatRect& rect);
 
 		/**
 		* Constructor using two vectors to describe the camera's view, the first 
 		* being the camera's center global coordinates, the second being the size
 		* of the camera
 		*/
-		Camera(sf::Vector2f center, sf::Vector2f size)
-		{
-			view.setCenter(center);
-			view.setSize(size);
-		}
+		Camera(const sf::Vector2f& center, const sf::Vector2f& size);
 
 		/**
 		* A defualt constructor, where the camera has zero size and center at the origin
 		*/
-		Camera()
-		{
-
-		}
+		Camera();
 
 		/**
 		* Method sets the view of the camera using a rectangle to describe its top left position
 		* and size
 		*/
-		void setView(sf::FloatRect rect);
+		void setView(const sf::FloatRect& rect);
+
+		/**
+		* Set the center of the camera's view using global cooridnates
+		*/
+		void setCenter(const sf::Vector2f& coordinates);
+
+		/**
+		* Set the size of the camera's view
+		*/
+		void setSize(const sf::Vector2f& size);
+
+		/**
+		* Rotate the view of the camera in degrees. Note, this sets the rotation,
+		* so using this method will always set the rotation relative to 0 degrees
+		*/
+		void setRotation(float angle);
+
+		/**
+		* rotate the view relative to the current orientation
+		*/
+		void rotate(float angle);
+
+		/**
+		* Move the camera based on its current location
+		*/
+		void move(float deltaX, float deltaY);
+
+		/**
+		* Method sets the view to part of the window. Thus the first
+		* value of the floatRect is the amount of offset from the right side of the screen
+		* second is the amount of offset from the left side of the screen
+		* the third is the amount of the x view it will show, the fourth is the amount of y view it will show
+		* These values must be from 0 to 1
+		*/
+		void setViewport(sf::FloatRect& rect);
+
+		/**
+		* Method takes a factor to zoom the screen out by. Thus a factor of less then 1
+		* zooms out, greater zooms in
+		*/
+		void zoom(float factor);
 	};
 }
 

499b5c6ce788ba0b7ccad9c015dfcec6b240f071
John Koehn <kingkoehn@gmail.com>
2014-07-08 23:34:57 -0500

    Added point class.

---
+++ Src/Galt-Engine/Point.h
@@ -0,0 +1,21 @@
+#ifndef VECTOR2I_H
+#define VECTOR2I_H
+
+#include "main.h"
+
+namespace GaltE
+{
+	template<class Type>
+
+	/**
+	* Class contains an x and y value
+	*/
+	class Vector2i
+	{
+	public:
+		Type x;
+		Type y;
+	};
+}
+
+#endif
